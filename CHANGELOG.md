e1301521c0711358b3fc09719bbafccb3b09f079
Michael Werle <micha@michaelwerle.com>
2020-06-22 22:51:08 +0900

    Update version and build artifacts for next release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [{VERSION}] for KSP {KSPVERSION} - {DATE}
+
+## [6.9.0.0] for KSP 1.9.1 - 2020-06-22
 ### Added
 - Docking port restrictions have been added. When cycling through docking ports,
   only docking ports which are compatible with the current vessel are shown. (#4)
--- Source/DockingPortAlignmentIndicator/Properties/AssemblyInfo.cs
+++ Source/DockingPortAlignmentIndicator/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DockingPortAlignmentIndicator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.8.5.1")]
-[assembly: AssemblyFileVersion("6.8.5.1")]
+[assembly: AssemblyVersion("6.9.0.0")]
+[assembly: AssemblyFileVersion("6.9.0.0")]
