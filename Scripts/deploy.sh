1d3a095e4eb495bb5562aee15ff56f27e81ed42c
Michael Werle <micha@michaelwerle.com>
2023-03-04 18:06:30 +0900

    chore(build): fix Deploy project for Linux and add helper scripts
    There's a bug/feature in msbuild for Linux which "helpfully" replaces
    backslashes with forward slashes while parsing project files. While in
    general this is useful for cross-platform compatibility with filenames,
    it is a major issue when using regular expressions or, indeed, any
    other situation in which a backslash is explicitly required.
    
    See : https://github.com/dotnet/msbuild/issues/3468

---
+++ Scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Builds the Release binaries and deploys them to your KSP Directory
+
+if ! command -v msbuild &>/dev/null
+then
+	echo Could not find 'msbuild' in the path. Please ensure it is installed properly.
+	exit 1
+fi
+
+cd "$(dirname "$0")"
+msbuild Deploy.proj /target:Deploy
+
